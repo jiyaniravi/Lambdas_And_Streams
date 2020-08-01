e11b4908c0c8871927a298650996451d50f13f77
Ravi Jiyani <jiyaniravi@gmail.com>
2020-08-01 20:14:12 +0530

    CourseFinder : Return courses having title length greater than 4 characters

--- src/main/java/com/af/CourseFinder.java
+++ src/main/java/com/af/CourseFinder.java
@@ -6,10 +6,11 @@ import java.util.List;
 
 public class CourseFinder {
     public static void main(String[] args) {
-        findCourse(new ArrayList<>(Arrays.asList("Spring","Spring Boot","API","AWS","Azure","Docker","Kubernetes")));
+        findCourse(new ArrayList<>(Arrays.asList("Spring","Spring Boot","API","AWS","Azure","PCF","Docker","Kubernetes")));
     }
 
     private static void findCourse(List<String> stringList){
-        stringList.stream().filter(course  -> course.contains("Spring")).forEach(System.out::println);
+        //stringList.stream().filter(course  -> course.contains("Spring")).forEach(System.out::println);
+        stringList.stream().filter(course  -> course.length() > 4).forEach(System.out::println);
     }
 }
