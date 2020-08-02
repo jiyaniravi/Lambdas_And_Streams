0c5332297ae747e2199c183fac1d80ac61fc85c7
Ravi Jiyani <jiyaniravi@gmail.com>
2020-08-02 18:35:17 +0530

    Stream : allMatch, noneMatch, anyMatch APIs

---
+++ src/main/java/com/af/CustomClass.java
@@ -0,0 +1,106 @@
+package com.af;
+
+import lombok.Builder;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+
+@Builder(setterPrefix = "with")
+class Course {
+    private String name;
+    private String category;
+    private int reviewScore;
+    private int noOfStudents;
+
+    public Course(String name, String category, int reviewScore, int noOfStudents) {
+        this.name = name;
+        this.category = category;
+        this.reviewScore = reviewScore;
+        this.noOfStudents = noOfStudents;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public int getReviewScore() {
+        return reviewScore;
+    }
+
+    public void setReviewScore(int reviewScore) {
+        this.reviewScore = reviewScore;
+    }
+
+    public int getNoOfStudents() {
+        return noOfStudents;
+    }
+
+    public void setNoOfStudents(int noOfStudents) {
+        this.noOfStudents = noOfStudents;
+    }
+
+    @Override
+    public String toString() {
+        return name+" : "+noOfStudents+" : "+reviewScore;
+    }
+}
+
+public class CustomClass {
+    public static void main(String[] args) {
+        List<Course> courses = new ArrayList<>(
+                Arrays.asList(
+                    new Course("Spring", "Framework", 98, 20000),
+                    new Course("Spring Boot", "Framework", 95, 18000),
+                    new Course("API", "Micro Services", 97, 22000),
+                    new Course("Micro Services", "Micro Services", 96, 25000),
+                    new Course("FullStack", "FullStack", 91, 14000),
+                    new Course("AWS", "Cloud", 92, 25000),
+                    new Course("Azure", "Cloud", 99, 21000),
+                    new Course("Docker", "Cloud", 92, 28000),
+                    Course.builder().withName("Kubernetes").withCategory("Cloud").withReviewScore(98).withNoOfStudents(19000).build()
+                )
+        );
+
+        Predicate<Course> reviewScoreGreaterThan95Predicate = course -> course.getReviewScore() > 95;
+        Predicate<Course> reviewScoreGreaterThan90Predicate = course -> course.getReviewScore() > 90;
+        Predicate<Course> reviewScoreLessThan90Predicate    = course -> course.getReviewScore() < 90;
+
+        System.out.println(
+                courses.stream().allMatch(
+                        reviewScoreGreaterThan95Predicate
+                )
+        );
+
+        System.out.println(
+                courses.stream().noneMatch(
+                        reviewScoreGreaterThan95Predicate
+                )
+        );
+
+        System.out.println(
+                courses.stream().noneMatch(
+                        reviewScoreLessThan90Predicate
+                )
+        );
+
+        System.out.println(
+                courses.stream().anyMatch(
+                        reviewScoreGreaterThan95Predicate
+                )
+        );
+    }
+}
